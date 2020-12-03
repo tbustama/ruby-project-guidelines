a871afa0404739d471844ed30f81dbceb88e50ae
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-12-02 20:04:33 -0800

    added city names

--- app/models/team_controller.rb
+++ app/models/team_controller.rb
@@ -6,10 +6,44 @@ class TeamController
 
     def add_to_database
         Team.destroy_all
+        city_names = {
+            "ARI"=> "Arizona",
+            "ATL"=> "Atlanta",
+            "BAL"=> "Baltimore",
+            "BUF"=> "Buffalo",
+            "CAR"=> "Charlotte",
+            "CHI"=> "Chicago",
+            "CIN"=> "Cincinnati",
+            "CLE"=> "Cleveland",
+            "DAL"=> "Dallas",
+            "DEN"=> "Denver",
+            "DET"=> "Detroit",
+            "GB" => "Green Bay",
+            "HOU"=> "Houston",
+            "IND"=> "Indianapolis",
+            "JAX"=> "Jacksonville",
+            "KC" => "Kansas City",
+            "LAC"=> "Los Angeles",
+            "LAR"=> "Los Angeles",
+            "LV" => "Las Vegas",
+            "MIA"=> "Miami",
+            "MIN"=> "Minneapolis",
+            "NE" => "New England",
+            "NO" => "New Orleans",
+            "NYG"=> "New York",
+            "NYJ"=> "New York",
+            "PHI"=> "Philadelphia",
+            "PIT"=> "Pittsburgh",
+            "SEA"=> "Seattle",
+            "SF" => "San Francisco",
+            "TB" => "Tamba Bay",
+            "TEN"=> "Nashville",
+            "WAS"=> "Washington D.C."
+        }
         @team_data.each {|team_hash|
             arguments = {
                 "team_name" => team_hash["TeamName"],
-                "team" => team_hash["Team"],
+                "team" => city_names[team_hash["Team"]],
                 "score" => team_hash["Score"],
                 "opponent_score" => team_hash["OpponentScore"],
                 "time_of_possession" => team_hash["TimeOfPossession"],
