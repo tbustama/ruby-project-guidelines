3f6e9a16ff1daf9360d08fba3259c60bf5ea5fc3
Molly <dolanmollye@gmail.com>
2020-12-02 16:31:29 -0800

    merge conflicts resolved' git status exit

--- db/migrate/20201202191359_create_users.rb
+++ db/migrate/20201202191359_create_users.rb
@@ -1,15 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-<<<<<<< HEAD
       t.string :name
       t.integer :age
       t.string :city
       t.string :motto
-=======
-      t.string :username
-      t.string :city
->>>>>>> 73767303e0690d7f1409cfbc20dd44da24f0345e
     end
   end
 end
--- db/migrate/20201202193304_create_teams.rb
+++ db/migrate/20201202193304_create_teams.rb
@@ -2,7 +2,6 @@ class CreateTeams < ActiveRecord::Migration[5.2]
   def change
     create_table :teams do |t|
       t.string :nickname
-<<<<<<< HEAD
       t.string :full_name
       t.string :location
       t.string :conference
@@ -14,7 +13,5 @@ class CreateTeams < ActiveRecord::Migration[5.2]
       t.integer :division_rank
       t.text :rivals
     end
-=======
->>>>>>> 73767303e0690d7f1409cfbc20dd44da24f0345e
   end
 end
