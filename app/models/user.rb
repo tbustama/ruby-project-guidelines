0aad48919885f5c3e3214250209c041c514a1b42
Molly <dolanmollye@gmail.com>
2020-12-03 14:24:21 -0800

    models added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,25 +3,39 @@ class User < ActiveRecord::Base
     has_many :teams, through: :favorite_teams
 
     def users_in_my_city
-        User.all.select{|user|
-            user.city == self.city
-        }
+        User.fans(self.city).delete(self)
     end
 
     def fellow_fans_in_my_city
-        self.users_in_my_city.select { |user|
-            user.teams.any? { |team|
-                self.teams.include?(team)
-            }
+        self.users_in_my_city.select {|user| 
+            self.fellow_fans.include?(user)
         }
     end
 
-    def self.fair_weather
-
+    def self.user_with_most_favorites
+        self.all.max_by {|user|
+            user.teams.count
+        }
     end
 
-    def fellow_fans(city)
-        
+    def self.fans(city)
+        User.all.select{|user|
+            user.city == city
+        }
+    end
+
+    def fellow_fans
+        User.all.find_all { |user|
+            user.teams.any? { |team|
+                self.teams.include?(team)
+            }
+        }.delete(self)
+    end
+
+    def fans_in_visiting_city(city)
+        User.fans(city).select {|user|
+            self.fellow_fans.include?(user)
+        }
     end
 
     
