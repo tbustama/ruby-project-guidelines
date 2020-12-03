76a960c02c6a7d6d7cea2fc63f7ab1399f6c05d4
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-12-03 15:15:12 -0800

    done

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,34 +3,40 @@ class Team < ActiveRecord::Base
     has_many :users, through: :favorite_teams
 
     def self.most_liked_team
-
+        Team.all.max_by{|team| team.favorite_teams.count}
     end
 
     def self.least_liked_team
-
+        Team.all.min_by{|team| team.favorite_teams.count}
     end
 
     def fan_average_age
-        
+        self.users.sum{|user| user.age} / self.users.count
     end
 
-    def self.geriatrics
-
+    def self.oldest_fans
+        Team.has_fans.max_by{|team| team.fan_average_age}
     end
 
-    def self.daycare
-        
+    def self.youngest_fans
+        Team.has_fans.min_by{|team| team.fan_average_age}
     end
     
-    def self.top_team
-
+    def self.top_scoring_team
+        Team.all.max_by{|team| team.score}
     end
 
-    def self.worst_team
-
+    def self.most_points_allowed
+        Team.all.max_by{|team| team.opponent_score}
     end
 
-    def self.division_leaders
-    
+    def self.most_widespread_fanbase
+        Team.all.max_by{|team| 
+            team.users.map{|user| user.city}.uniq.count
+        }
+    end
+
+    def self.has_fans
+        Team.all.select{|team| team.users.count != 0}
     end
 end
