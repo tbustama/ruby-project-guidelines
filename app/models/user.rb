9d0ccc1ccd33896b307f15e0c61d7a4148862ae5
Molly <dolanmollye@gmail.com>
2020-12-03 10:53:39 -0800

    added two methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,17 @@ class User < ActiveRecord::Base
     has_many :teams, through: :favorite_teams
 
     def users_in_my_city
-
+        User.all.select{|user|
+            user.city == self.city
+        }
     end
 
     def fellow_fans_in_my_city
-
+        self.users_in_my_city.select { |user|
+            user.teams.any? { |team|
+                self.teams.include?(team)
+            }
+        }
     end
 
     def self.fair_weather
