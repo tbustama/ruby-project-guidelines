50fd700ff7a55a4936c343bc39464c88eba0ab5c
Molly <dolanmollye@gmail.com>
2020-12-03 14:45:24 -0800

    updated user methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ActiveRecord::Base
     has_many :teams, through: :favorite_teams
 
     def users_in_my_city
-        User.fans(self.city).delete(self)
+        users = User.users_in_city(self.city)
+        users.delete(self)
+        users
     end
 
     def fellow_fans_in_my_city
@@ -18,24 +20,34 @@ class User < ActiveRecord::Base
         }
     end
 
-    def self.fans(city)
+    def self.users_in_city(city)
         User.all.select{|user|
             user.city == city
         }
     end
 
     def fellow_fans
-        User.all.find_all { |user|
+        users = User.all.find_all { |user|
             user.teams.any? { |team|
                 self.teams.include?(team)
             }
-        }.delete(self)
+        }
+        users.delete(self)
+        users
     end
 
     def fans_in_visiting_city(city)
-        User.fans(city).select {|user|
+        User.users_in_city(city).select {|user|
             self.fellow_fans.include?(user)
         }
+    end
+
+    def self.oldest_user
+        self.all.max_by{|user| user.age}
+    end
+
+    def self.youngest_user
+        self.all.min_by{|user| user.age}
     end
 
     
