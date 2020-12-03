7ec01760c0a02c1d0aa6373579084e3edf777c9d
Molly <dolanmollye@gmail.com>
2020-12-02 16:54:07 -0800

    updated Teams table

--- db/migrate/20201202193304_create_teams.rb
+++ db/migrate/20201202193304_create_teams.rb
@@ -1,17 +1,18 @@
 class CreateTeams < ActiveRecord::Migration[5.2]
   def change
     create_table :teams do |t|
-      t.string :nickname
-      t.string :full_name
-      t.string :location
-      t.string :conference
-      t.string :division
-      t.string :venue
-      t.integer :wins
-      t.integer :losses
-      t.integer :ties
-      t.integer :division_rank
-      t.text :rivals
+      t.string :team
+      t.string :team_name
+      t.integer :score
+      t.integer :opponent_score
+      t.datetime :time_of_possession
+      t.integer :first_downs
+      t.integer :penalties
+      t.integer :fumbles
+      t.integer :fumbles_forced
+      t.integer :passing_touchdowns
+      t.integer :rushing_touchdowns
+      t.integer :sacks
     end
   end
 end
