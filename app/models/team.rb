8a6d501290a2eaae3bf61ce013a6b85687a32d5f
Lucas Montanari <Lmonty2011@gmail.com>
2020-12-07 09:55:32 -0800

    added color to responses

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,13 +4,13 @@ class Team < ActiveRecord::Base
 
     def self.most_liked_team
         team_name = Team.all.max_by{|team| team.favorite_teams.count}
-        puts "The #{team_name.name} are the most liked team in the NFL!"
+        puts "The #{team_name.name} are the most liked team in the NFL!".red
         team_name
     end
 
     def self.least_liked_team
         team_name = Team.all.min_by{|team| team.favorite_teams.count}
-        puts "The #{team_name.name} are the least liked team in the NFL. Someone call their marketing team..."
+        puts "The #{team_name.name} are the least liked team in the NFL. Someone call their marketing team...".red
         team_name
     end
 
@@ -20,13 +20,13 @@ class Team < ActiveRecord::Base
 
     def self.oldest_fans
         old_team = Team.has_fans.max_by{|team| team.fan_average_age}
-        puts "Respect your elders! #{old_team.name} have the oldest fans with an average of #{old_team.fan_average_age} years old."
+        puts "Respect your elders! #{old_team.name} have the oldest fans with an average of #{old_team.fan_average_age} years old.".red
         old_team
     end
 
     def self.youngest_fans
         young_team = Team.has_fans.min_by{|team| team.fan_average_age}
-        puts "#{young_team.name} have the youngest fans in the NFL. First 500 fans at the next game get a pacifier!"
+        puts "#{young_team.name} have the youngest fans in the NFL. First 500 fans at the next game get a pacifier!".red
         young_team
     end
     
@@ -36,7 +36,7 @@ class Team < ActiveRecord::Base
 
     def self.puts_top_scoring
         top_team = self.top_scoring_team
-        puts "The #{top_team.name} are the top scoring team in the NFL. Get that offensive coordinator a raise!"
+        puts "The #{top_team.name} are the top scoring team in the NFL. Get that offensive coordinator a raise!".red
     end
 
     def self.worst_scoring_team
@@ -45,12 +45,12 @@ class Team < ActiveRecord::Base
 
     def self.puts_worst_scoring
         worst_team = self.worst_scoring_team
-        puts "The #{worst_team.name} have the worst offense in the NFL. Taking bets on who gets fired first."
+        puts "The #{worst_team.name} have the worst offense in the NFL. Taking bets on who gets fired first.".red
     end
 
     def self.most_points_allowed
         worst_team = Team.all.max_by{|team| team.opponent_score}
-        puts "The Defensive Coordinator of the #{worst_team.name} needs to be fired."
+        puts "The Defensive Coordinator of the #{worst_team.name} needs to be fired.".red
         worst_team
     end
 
@@ -58,7 +58,7 @@ class Team < ActiveRecord::Base
         widespread = Team.all.max_by{|team| 
             team.users.map{|user| user.city}.uniq.count
         }
-        puts "The #{widespread.name} fanbase is the most spread out of any other fanbase!"
+        puts "The #{widespread.name} fanbase is the most spread out of any other fanbase!".red
         widespread
     end
 
@@ -69,19 +69,19 @@ class Team < ActiveRecord::Base
     def users_mottos
         mottos = self.users.map{|user| user.motto}
         if mottos != []
-            mottos.each_with_index {|motto, i| puts "#{i + 1}. #{motto} "}
+            mottos.each_with_index {|motto, i| puts "#{i + 1}. #{motto} ".red}
         else  
-            puts "You have no fans... your motto should be 'get some fans'"
+            puts "You have no fans... your motto should be 'get some fans'".red
         end
     end
 
     def self.top_team_mottos
-        puts "Here are the best team's fan's mottos:"
+        puts "Here are the best team's fan's mottos:".red
         self.top_scoring_team.users_mottos
     end
 
     def self.worst_team_mottos
-        puts "Here are the worst team's fans mottos (who cares):"
+        puts "Here are the worst team's fans mottos (who cares):".red
         self.worst_scoring_team.users_mottos
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,11 @@ class User < ActiveRecord::Base
 
     def print_users_in_my_city
         users = self.users_in_my_city
-        puts "These are the names of people in your city:"
+        puts "These are the names of people in your city:".red
         users.each{|user| 
-            print user.name
+            print "#{user.name}".red
             if users.last != user 
-                print ", "
+                print ", ".red
             end
         }
         print "\n"
@@ -24,13 +24,13 @@ class User < ActiveRecord::Base
             self.fellow_fans.include?(user)
         }
         if fellow_fans.count == 0
-            puts "Sorry you're alone on this one."
+            puts "Sorry you're alone on this one.".red
         else 
-            puts "You've found your people!"
+            puts "You've found your people!".red
             fellow_fans.each{ |fan| 
-                print fan.name
+                print "#{fan.name}".red
                 if fellow_fans.last != fan 
-                    print ", "
+                    print ", ".red
                 end
             }
         print "\n"
@@ -42,7 +42,7 @@ class User < ActiveRecord::Base
         bandwagon = self.all.max_by {|user|
             user.teams.count
         }.name
-        puts "Always a winner when you're on the bandwagon. Right, #{bandwagon}?"
+        puts "Always a winner when you're on the bandwagon. Right, #{bandwagon}?".red
     end
 
     def self.users_in_city(city)
@@ -65,13 +65,13 @@ class User < ActiveRecord::Base
             self.fellow_fans.include?(user)
         }
         if fans.count == 0
-            puts "Looks like you'll have to start your own fanbase"
+            puts "Looks like you'll have to start your own fanbase".red
         else 
-            puts "New in town? Get together with your fellow fans and cheer!"
+            puts "New in town? Get together with your fellow fans and cheer!".red
             fans.each{|fan| 
-            print fan.name
+            print "#{fan.name}".red
             if fans.last != fan 
-                print ", "
+                print ", ".red
             end
         }
         print "\n"
@@ -81,23 +81,23 @@ class User < ActiveRecord::Base
 
     def self.oldest_user
         oldest = User.where(age: User.maximum("age"))[0].name
-        puts "The award for the most trips around the sun goes to #{oldest}!"
+        puts "The award for the most trips around the sun goes to #{oldest}!".red
     end
 
     def self.youngest_user
         youngest = User.where(age: User.minimum("age"))[0].name
-        puts "The Golden Pacifier goes to #{youngest}!"
+        puts "The Golden Pacifier goes to #{youngest}!".red
     end
 
     def favorite_teams_cities
         self.teams.each { |team| 
-            puts "Cities for fans of team: #{team.name}"
+            puts "Cities for fans of team: #{team.name}".red
             city_array = team.user_cities
             city_array.each { |city| 
                 if city != city_array.last
-                    print city + ", "
+                    print "#{city}, ".red
                 elsif
-                    puts city
+                    puts "#{city}".red
                 end
             }
         }
